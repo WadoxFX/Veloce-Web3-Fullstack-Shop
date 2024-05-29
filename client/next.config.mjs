8db6af008879034ee3aa9e5978814115e18261ba
WadoxFX <chkolakvkv45kv@gmail.com>
2024-05-29 22:05:42 +0300

    🧪 Project setup

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     SERVER_URL: 'http://localhost:8080/',
-    CONTRACT_URL: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+    CONTRACT_URL: 'Youre contract url',
   },
   images: { domains: ['localhost', '192.168.1.4'] },
 }
